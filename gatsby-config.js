d364bc96a773139eed21c651a96d51d32542b5c9
Bryce Pedroza <brycepedroza@gmail.com>
2021-06-07 21:22:45 -0700

    testing out removing background css stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
       options: {
         name: `Bryce Pedroza - A Personal Website`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `Bryce Pedroza Personal Website`,
         start_url: `/`,
         theme_color: `#6B46C1`,
         display: `standalone`,
